247a30b268b1413cb10dc01a4757442e82dfcb19
Marta Taulet <mta@odoo.com>
2021-01-11 12:17:46 -0800

    [ADD] pos_cashbox_oybi

---
+++ pos_cashbox_oybi/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pos_cashbox_oybi",
+
+    'summary': """
+        PoS Open Cashbox""",
+
+    'description': """
+        Own Your BI: Open Cashbox on Closed PoS Sessions
+    """,
+    'author': "Odoo Inc",
+    'website': "http://www.odoo.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'OEEL-1',
+    'depends': ['base', 'pos_iot'],
+    'data': [
+        'views/views.xml',
+    ],
+}
