485a698e341652199d5b484562b24d9b1570e5d5
Marta Taulet <mta@odoo.com>
2021-01-27 11:54:10 -0800

    [REF] Use javascript EpsonPrinter module

--- pos_cashbox_oybi/models/pos.py
+++ pos_cashbox_oybi/models/pos.py
@@ -3,7 +3,6 @@
 import logging
 
 from odoo import models
-from odoo.addons.hw_drivers.controllers.driver import iot_devices
 from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -11,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    def open_backend_cb(self):
+    def open_backend_cashdrawer(self):
         """Open the cashdrawer associated with this pos.config"""
 
         _logger.info('Opening Cashdrawer: Sending signal to printer...')
@@ -19,15 +18,11 @@ class PosConfig(models.Model):
         if not self:
             return False
         if not self.epson_printer_ip:
-            raise ValidationError('No printer configured on this PoS Station. Please set up the IP address of the printer in Settings.')
+            raise ValidationError('No printer configured on this PoS Station. Please set up the IP address of the printer in the session\'s Settings.')
 
-        data = {'action': 'cashbox'}
-
-        printer = next((d for d in iot_devices if iot_devices[d].device_type == 'printer' \
-                                and iot_devices[d].device_connection == 'direct' \
-                                and iot_devices[d].iot_ip == self.epson_printer_ip), None)
-        if printer:
-            iot_devices[printer].action(data)
-            return True
-
-        raise ValidationError('Connection to the printer failed. Please check if the printer is still connected.')
+        return {
+            'type': 'ir.actions.client',
+            'target': 'current',
+            'tag': 'pos_cashbox_oybi.cashbox_backend',
+            'params': {'ip': self.epson_printer_ip}
+        }
